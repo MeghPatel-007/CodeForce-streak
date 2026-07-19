29945a7437bd209e7ebcd2038a96e39536369553
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-07-19 17:32:13 +0530

    day-10

---
+++ july/day-10-158A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n;
+    cin >> k;
+    int cnt = 0;
+    vector<int> v(n);
+    for (auto &a : v)
+    {
+        cin >> a;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] >= v[k - 1] && v[i] != 0)
+            cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
